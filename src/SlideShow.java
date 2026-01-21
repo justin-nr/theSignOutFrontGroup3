95a66d75ffcb29ffe89f5ab6bdb6bff8b74fc074
Pacer <crate0diggers@gmail.com>
2026-01-21 15:40:31 -0800

    -Added more functionality to the LinkedList, SlideShow is now usable, each node has an uptime now.

--- src/SlideShow.java
+++ src/SlideShow.java
@@ -1,6 +1,16 @@
 public class SlideShow {
+    int slideNumber;
+    LinkedList list;
     SlideShow() {
-
+        slideNumber = 0;
+        list = new LinkedList();
+    }
+    SlideShow(String[] intdata, int uptime) {
+        list = new LinkedList();
+        for (int i = 0; i < intdata.length; i++) {
+            String toAdd = intdata[i];
+            list.append(toAdd, uptime);
+        }
     }
 }
 
@@ -10,34 +20,84 @@ class LinkedList {
     // Empty Constructor.
     LinkedList() {}
     // Constructor with base info.
-    LinkedList(String contents){
-        head = new Node(contents);
+    LinkedList(String contents, int upTime){
+        head = new Node(contents, upTime);
         head.next = head;
     }
 
     // Appends the LinkedList with new info.
-    public void append(String contents) {
+    public void append(String contents, int upTime) {
         if (head != null) {
             Node currentNode = head;
             while (currentNode.next != head) {
                 currentNode = currentNode.next;
             }
-            Node node = new Node(contents);
+            Node node = new Node(contents, upTime);
             currentNode.next = node;
             node.next = head;
         } else {
-            head = new Node(contents);
+            head = new Node(contents, upTime);
             head.next = head;
         }
+    }
+
+    public void insert(String contents, int position, int upTime) {
+        Node currentNode = head;
+        Node lastNode = null;
+        int pos = 0;
+
+        while (currentNode.next != null && currentNode.next != head && pos != position) {
+            lastNode = currentNode;
+            currentNode = currentNode.next;
+            pos++;
+        }
+
+        Node newNode = new Node(contents, upTime);
+        lastNode.next = newNode;
+        newNode.next = currentNode;
+    }
+
+    public void connectEndToHead() {
+        Node current = head;
+        while (current.next != head && current.next != null) {
+            current = current.next;
+        }
+        current.next = head;
+    }
+
+    public int getNodeCount() {
+        int value = 0;
+        Node currentNode = head;
+
+        while (currentNode.next != null && currentNode.next != head) {
+            value += 1;
+            currentNode = currentNode.next;
+        }
+
+        value += 1;
+
+        return value;
+    }
+
+    public Node getLastNode() {
+        Node currentNode = head;
+
+        while (currentNode.next != null && currentNode.next != head) {
+            currentNode = currentNode.next;
+        }
+
+        return  currentNode;
     }
 }
 
 class Node {
     Node next;
     String data;
+    int upTime;
 
     // Node constructor.
-    Node(String contents) {
+    Node(String contents, int slideUpTime) {
+        upTime = slideUpTime;
         data = contents;
     }
 }
