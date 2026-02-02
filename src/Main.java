d3a36121386dc67c0ee6f0ee74d8d58080a1a18c
Pacer <crate0diggers@gmail.com>
2026-02-02 13:35:18 -0800

    Merged Sairus' changes with Riley's

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import Screen.Screen;
 
 public class Main {
     public static void main(String[] args) {
-//        Queue queue = new Queue(new PersonNode(PersonRole.STUDENT));
-//        queue.append(new PersonNode(PersonRole.STUDENT));
+        Queue queue = new Queue(new PersonNode(PersonRole.STUDENT));
+        queue.append(new PersonNode(PersonRole.STUDENT));
 
         //shows window
         Screen screen = new Screen();
